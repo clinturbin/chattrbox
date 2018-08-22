6659fcd6026a77fd55e3303aa583d86c766687b6
clinturbin <clinturbin@gmail.com>
2018-08-22 18:35:31 -0400

    Added function to ask for user name when first connecting

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 console.log("Make me do things!");
 
-// Create Web Socket Connection
 let socket = new WebSocket('ws://localhost:3001');
 let $messageContainer = $('.chat-message-list');
 let messageInput = document.querySelector("input[data-chat='message-input']");
 let sendButton = document.querySelector(".btn-default");
-let userName = prompt("What is your username?");
-let userList = document.querySelector(".user-list");
+let userList = document.querySelector('.user-list');
 
 let drawMessage = ({
     user: u,
@@ -50,34 +48,55 @@ let $messageRow = drawMessage({
 
 $messageRow.get(0).scrollIntoView();
 
+let askForUserName = () => {
+  let $messageRow = $('<li>');
+  let $message = $('<p>');
+  $message.append($('<span>', {
+    'class': 'message-message',
+    text: 'What is your user name?'
+  }));
+  $messageRow.append($message);
+  $messageContainer.append($messageRow);
+}
+
+let addUserToParticipants = (message) => {
+  let userRow = document.createElement("li");
+  userRow.setAttribute('data-user', message.user);
+  userRow.textContent = message.user;
+  userList.appendChild(userRow);
+};
+
+let join = (message) => {
+  addUserToParticipants(message);
+};
+
+let leave = (message) => {
+  let userRow = document.querySelector(`[data-user="${message.user}`);
+  userList.removeChild(userRow);
+};
+
+let message = (message) => {
+  let messageObject = {
+    user: message.user,
+    message: message.body,
+    timestamp: new Date()
+  };
+  $messageContainer.append(drawMessage(messageObject));
+};
+
+let routes = {join, leave, message};
+
 // Connection Opened
 socket.addEventListener('open', (event) => {
-  addToParticipants();
-  sendButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    let messageObject = makeMessageObject();
-    socket.send(JSON.stringify(messageObject));
+  askForUserName();
+  sendButton.addEventListener("click", () => {
+    socket.send(messageInput.value);
   });
 });
 
 // Listen for messages
 socket.addEventListener('message', (event) => {
-  $messageContainer.append(drawMessage(JSON.parse(event.data)));
+  let message = JSON.parse(event.data);
+  console.log(message);
+  routes[message.type](message);
 });
-
-// socket.addEventListener('close', (event) => )
-
-let makeMessageObject = () => {
-  let messageObject = {
-    user: userName,
-    message: messageInput.value,
-    timestamp: new Date()
-  };
-  return messageObject;
-};
-
-let addToParticipants = () => {
-  let userListItem = document.createElement("li");
-  userListItem.textContent = userName;
-  userList.appendChild(userListItem);
-};
--- webSockets.js
+++ webSockets.js
@@ -2,31 +2,36 @@ const WS = require('ws');
 
 let webSocket = new WS.Server({port: 3001});
 
-webSocket.on('connection', sender => {
+
+let sendAll = (message) => {
+    webSocket.clients.forEach(client => {
+        client.send(message);
+    });
+};
+
+let joinNotification = (user) => ({type: 'join', user});
+let leaveNotification = (user) => ({type: 'leave', user});
+let messageNotification = (user, body) => ({type: 'message', user, body});
+
+let activeUsers = new Set();
+
+webSocket.on('connection', client => {
     console.log("Hey there! Someone connected");
-    sender.on('message', message => {
-        webSocket.clients.forEach(client => {
-            client.send(message);
-        });
+    activeUsers.forEach(user => {
+        client.send(JSON.stringify(joinNotification(user)));
+    });
+    let userName;
+    client.on('close', () => {
+        sendAll(JSON.stringify(leaveNotification(userName)));
+        activeUsers.delete(userName);
+    });
+    client.on('message', message => {
+        if (!userName) {
+            userName = message;
+            sendAll(JSON.stringify(joinNotification(userName)));
+            activeUsers.add(userName);
+        } else {
+            sendAll(JSON.stringify(messageNotification(userName, message)));
+        }
     });
 });
-
-// Alternate way to ask for 
-
-// webSocket.on('connection', sender => {
-//     console.log("Hey there! Someone connected");
-//     sender.send("What is your username");
-//     let username;
-//     sender.on('message', message => {
-//         if (!username) {
-//             username = message;
-//             webSocket.clients.forEach(client => {
-//                 client.send(message + " has joined!");
-//             });
-//         } else {
-//             webSocket.clients.forEach(client => {
-//                 client.send(username + ": " + message);
-//             });    
-//         }
-//     });
-// });
